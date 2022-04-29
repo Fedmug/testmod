22da8f125fbdd640c406f07e8fb23a5bf4573331
Sergey Lytkin <smlytkin@gmail.com>
2022-04-29 22:21:19 +0300

    hello: initial commit

---
+++ hello.go
@@ -0,0 +1,15 @@
+package hello
+
+import "rsc.io/quote/v3"
+
+func Hello() string {
+    return quote.HelloV3()
+}
+
+func Proverb() string {
+    return quote.Concurrency()
+}
+
+func Go() string {
+    return quote.GoV3()
+}
